1a1d2def74d119a95d0d48a35804556d8571338c
noergm3 <noergm3@gmail.com>
2026-01-03 20:17:16 -0600

    Se agrego Site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://noegonzalez.dev',
+    generateRobotsTxt: true,
+    changefreq: 'weekly',
+    priority: 0.8,
+    sitemapSize: 5000,
+};
